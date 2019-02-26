47496f70227f4321583b5c43d9cb9bb35d71454f
Joshua Landry and Lex Wilkins <joshua+lex@>
2019-02-25 20:49:21 -0800

    broken code will fix after push to remote repo

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,42 @@
 $(document).ready(function(){
 
-  $('form#form1').submit(function(){
-    var q1 = $("input#q1").val();
-    var q2 = $("input#q2").val();
-    var q3 = $("input#q3").val();
-    var q4 = $("input#q4").val();
+  $('form#form1').submit(function(event){
 
-    var list1 = [q1, q2, q3, q4];
-    // console.log(list1);
+
+    var questions = ['1', '2', '3', '4'];
+    var userInputs = [];
+
+    questions.forEach(function(question){
+      var userInput = $('input#q' + question).val();
+      $('#thing' + question).text(userInput);
+
+      console.log(userInput);
+      userInputs.push(userInput);
+      // console.log(question);
+
+    });
+    console.log(userInputs);
+
+    // var q1 = $("input#q1").val();
+    // var q2 = $("input#q2").val();
+    // var q3 = $("input#q3").val();
+    // var q4 = $("input#q4").val();
+    // var list1 = [q1, q2, q3, q4];
+
 
     var newList = [];
-    newList.push([list1[1]]);
-    newList.push([list1[0]]);
-    newList.push([list1[2]]);
-    // console.log(newList);
+
+
+
+    newList.push([userInputs[1]]);
+    newList.push([userInputs[0]]);
+    newList.push([userInputs[2]]);
+    console.log(newList);
+
+
+    newList.forEach(function(input){
+
+    });
 
     $('#thing1').append(newList[0]);
     $('#thing2').append(newList[1]);
